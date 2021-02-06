422b05f80b3f04eba4852aa039b82871a41b516f
zorana98 <lazic.ra53.2017@uns.ac.rs>
2021-02-06 02:18:59 +0100

    refactor: added buttons on employee profile

--- jpa-example/src/main/resources/static/js/dermatologist.js
+++ jpa-example/src/main/resources/static/js/dermatologist.js
@@ -1,28 +1,42 @@
 Vue.component("employee",{
 	data: function () {
 		    return {
-				employee: null  
+				employee: null,
+				mode: 'BROWSE',
+				backup: []
 			}				
 }, 
-template: `
-	<div>
-		<table>
-		<tr><th>HOME PAGE FOR:   {{employee.userType}}</th></tr>
-		<tr><td>FirstName:</td><td>{{employee.firstName}}</td></tr>
-		<tr><td>LastName:</td><td>{{employee.lastName}}</td><tr>
-		<tr><td>PhoneNumber:</td><td>{{employee.phoneNumber}}</td><tr>
-		<tr><td>Street:</td><td>{{employee.address.street}}</td></tr>
-		<tr><td>StreetNumber</td><td>{{employee.address.streetNumber}}</td></tr>
-		<tr><td>City</td><td>{{employee.address.city}}</td></tr>
-		<tr><td>Country</td><td>{{employee.address.country}}</td></tr>
-		<tr><td>E-mail:</td><td>{{employee.email}}</td><tr>
-		<tr><td>Password:</td><td>{{employee.password}}</td><tr>		
-		</table>
-	</div>
-	`,
+	template: `
+		<div>
+			<table>
+				<tr><th>HOME PAGE FOR:   {{employee.userType}}</th></tr>
+				<tr><th>FirstName:</th><td><input type = "text" v-model= "employee.firstName"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+				<tr><th>LastName:</th><td><input type = "text" v-model= "employee.lastName"  v-bind:disabled = "mode != 'EDIT'"/></td><tr>
+				<tr><th>PhoneNumber:</th><td><input type = "text" v-model= "employee.phoneNumber"  v-bind:disabled = "mode != 'EDIT'"/></td><tr>
+				<tr><th>Street:</td><th><input type = "text" v-model= "employee.address.street"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+				<tr><th>StreetNumber</th><td><input type = "text" v-model= "employee.address.streetNumber"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+				<tr><th>City</th><td><input type = "text" v-model= "employee.address.city"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+				<tr><th>Country</th><td><input type = "text" v-model= "employee.address.country"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+				<tr><th>E-mail:</th><td>{{employee.email}}</td><tr>
+				<tr><th>Password:</th><td><input type = "text" v-model= "employee.password" v-bind:disabled = "mode != 'EDIT'"/></td><tr>
+				<tr>
+					<td><button v-on:click = "editEmployee" type="button" v-bind:disabled = "mode != 'BROWSE'">Edit</button></td>
+					<td><button v-on:click = "saveEmployee" type="button" v-bind:disabled = "mode != 'EDIT'">Save</button></td>
+				</tr>	
+			</table>
+		</div>
+		`,
 	mounted() {
 		axios
 		.get('users/1')
 		.then(response => (this.employee = response.data));
+	},
+	methods: {
+		editEmployee : function () {
+			this.mode = "EDIT";
+		},
+		saveEmployee : function () {
+			this.mode = "BROWSE";
+		}
 	}
 });
