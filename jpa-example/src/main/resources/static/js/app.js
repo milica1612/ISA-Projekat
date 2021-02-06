7cb25fbee1273559c40ae2b135b1473888c0c164
Ivana Gavric <ivanagavric1998@uns.ac.rs>
2021-02-06 15:57:00 +0100

    feat: add supplier profile, edit personal data

--- jpa-example/src/main/resources/static/js/app.js
+++ jpa-example/src/main/resources/static/js/app.js
@@ -1,9 +1,20 @@
 const employee = {template: '<employee></employee>'}
+const sysAdmin = {template: '<sysAdmin></sysAdmin>'}
+const register = {template: '<register></register>'}
+const logIn = {template: '<logIn></logIn>'}
+const supplier = {template: '<supplier></supplier>'}
+const addMedicines = {template: '<addMedicines></addMedicines>'}
+
 
 const router = new VueRouter({
 	mode: 'hash',
 	routes: [
-		{path: '/employee', component: employee}
+		{path: '/employee', component: employee},
+		{path: '/sysAdmin', component: sysAdmin},
+		{path: '/register', component: register},
+		{path: '/logIn', component: logIn},
+		{path: '/supplier', component: supplier},
+		{path: '/addMedicines', component: addMedicines}
 	]
 });
 
--- jpa-example/src/main/resources/static/js/supplier.js
+++ jpa-example/src/main/resources/static/js/supplier.js
@@ -1,38 +1,49 @@
 Vue.component("supplier", {
 	data: function () {
 		    return {
-		    	supplier: null
+		    	supplier: null,
+				mode: 'BROWSE',
+				backup: []
 		    }
 	},
 	template: `
-	<div id = "wrapper">
-		<h1>Supplier</h1>
-				
-		<div id = "app" v-on:click="seen = !seen" class="control">
-		<table>
-			<tr>
-			<th><button type="button" v-on:click="addMedicines" style="background-color:SlateBlue; border:none; padding: 15px 32px; font-size: 16px; display: inline-block;">Register Pharmacy</button></th>
-			
-			<th><button type="button"  style="background-color:SlateBlue; border:none;  padding: 15px 32px; font-size: 16px; display: inline-block;">Register Pharmacy Administrator</button></th>
-		    
-		    <th><button type="button"  style="background-color:SlateBlue; border:none;  padding: 15px 32px; font-size: 16px; display: inline-block;">Register Dermatologist</button></th>
-		    
-		    <th><button type="button"  style="background-color:SlateBlue; border:none;  padding: 15px 32px; font-size: 16px; display: inline-block;">Register Supplier</button></th>
-		    
-			<th><button type="button" v-on:click = "addMedicines" style="background-color:SlateBlue; border:none;  padding: 15px 32px; font-size: 16px; display: inline-block;">Add New Medicine</button></th>
-		    
-			<th><button type="button"  style="background-color:SlateBlue; border:none; padding: 15px 32px; font-size: 16px;display: inline-block; display: inline-block;">Answer On Complaint</button></th>
-			
-			<th><button type="button"  style="background-color:SlateBlue; border:none;  padding: 15px 32px; font-size: 16px; display: inline-block;">Loyalty Program</button></th>
-			
-			<th><button type="button"  style="background-color:SlateBlue; border:none;  padding: 15px 32px; font-size: 16px; display: inline-block;">Add New System Admin</button></th>
-		    </tr>
-		  </table>
-		    </div>
+	<div>
+		<table style = "margin: 0 auto;">
+		<tr><td><button style = "background-color:SlateBlue; padding: 10px; border: none; cursor: pointer; width:100%; opacity: 0.9;" type="button" v-bind:disabled = "mode != 'BROWSE'">See All Offers</button></td>
+		<td><button style = "background-color:SlateBlue; padding: 10px; border: none; cursor: pointer; width:250%; opacity: 0.9;" type="button" v-bind:disabled = "mode != 'BROWSE'">Create Offer</button></td></tr>
+		</table>
+		
+		<table style = "margin: 0 auto;">
+		<tr><h1 style = "text-align: center;">Supplier Profile</h1></tr>
+		<tr><th>First Name:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.firstName"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+		<tr><th>Last Name:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.lastName"  v-bind:disabled = "mode != 'EDIT'"/></td><tr>
+		<tr><th>Phone Number:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.phoneNumber"  v-bind:disabled = "mode != 'EDIT'"/></td><tr>
+		<tr><th>Street:</td><th><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.address.street"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+		<tr><th>Street Number:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.address.streetNumber"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+		<tr><th>City:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.address.city"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+		<tr><th>Country:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.address.country"  v-bind:disabled = "mode != 'EDIT'"/></td></tr>
+		<tr><th>E-mail:</th><td>{{supplier.email}}</td><tr>
+		<tr><th>Password:</th><td><input type = "text" style = "width : 100%; padding: 10px; border: none; background: #f1f1f1;" v-model= "supplier.password" v-bind:disabled = "mode != 'EDIT'"/></td><tr>
+		<tr>
+			<td><button v-on:click = "editSupplier" style = "background-color:SlateBlue; padding: 10px; border: none; cursor: pointer; width:100%; opacity: 0.9;" type="button" v-bind:disabled = "mode != 'BROWSE'">Edit</button></td>
+			<td><button v-on:click = "saveSupplier" style = "background-color:SlateBlue; padding: 10px; border: none; cursor: pointer; width:111%; opacity: 0.9;" type="button" v-bind:disabled = "mode != 'EDIT'">Save</button></td>
+		</tr>	
+	</table>
 </div>
 `,
 	mounted(){
 		axios
+		.get('users/1')
+		.then(response => (this.supplier = response.data));
+	},
+	methods: {
+		editSupplier : function () {
+			this.mode = "EDIT";
+		},
+		saveSupplier : function () {
+			this.mode = "BROWSE";
+			axios
+			.post("/application/users/update", this.supplier);
+		}
 	}
-	
 });
