8c29f668073a15c49f2ecd271ea035f8a9cd459c
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-02-10 17:33:18 +0100

    feat: added data-postgres scrip
    Added the initial script
    
    Co-Authored-By: ivanagavric98 <65288879+ivanagavric98@users.noreply.github.com>
    Co-Authored-By: milica1612 <57577588+milica1612@users.noreply.github.com>
    Co-Authored-By: zorana98 <57637627+zorana98@users.noreply.github.com>

---
+++ jpa-example/src/main/resources/data-postgres.sql
@@ -0,0 +1,72 @@
+INSERT INTO public.address(
+	address_id, city, country, latitude, longitude, street, street_number)
+	VALUES (1, 'Novi Sad', 'Srbija', 48, 44, 'Danila Kisa', 30);
+
+INSERT INTO public.address(
+	address_id, city, country, latitude, longitude, street, street_number)
+	VALUES (2, 'Novi Sad', 'Srbija', 48, 44, 'Branka Bajića', 20);	
+	
+INSERT INTO public.address(
+	address_id, city, country, latitude, longitude, street, street_number)
+	VALUES (3, 'Novi Sad', 'Srbija', 48, 44, 'Bulevar kralja Petra I', 25);	
+
+INSERT INTO public.address(
+	address_id, city, country, latitude, longitude, street, street_number)
+	VALUES (4, 'Novi Sad','Srbija', 48, 44, 'Alekse Santica', 46);
+	
+INSERT INTO public.address(address_id, city, country, latitude, longitude, street, street_number)
+	VALUES (5, 'Novi Sad','Srbija', 48, 44, 'Alekse Santica', 11);
+	
+INSERT INTO public.address(
+	address_id, city, country, latitude, longitude, street, street_number)
+	VALUES (6, 'Novi Sad','Srbija', 48, 44, 'Kisacka', 3);
+
+INSERT INTO public.pharmacy(
+	pharmacy_id, name, rating)
+	VALUES (DEFAULT, 'Pharmacy 1', 8.1);
+	
+INSERT INTO public.medicine_specification(
+	med_spec_id, dosage)
+	VALUES (1, 4);
+	INSERT INTO public.medicine_specification(
+	med_spec_id, dosage)
+	VALUES (2, 3);
+	INSERT INTO public.medicine_specification(
+	med_spec_id, dosage)
+	VALUES (3, 3);
+	
+INSERT INTO public.medicine(
+	medicine_id, loyalty_points, manufacturer, code, medicine_form, name, precautions, prescription_type, type, medicine_specification_med_spec_id)
+	VALUES (1, 10, 'Hemofarm', 'TR9NF','PILL', 'brufen', 'nema', 'NO_PRESCRIPTION', 'analgetik', 1);
+	
+INSERT INTO public.medicine(
+	medicine_id, loyalty_points, manufacturer, code, medicine_form, name, precautions, prescription_type, type, medicine_specification_med_spec_id)
+	VALUES (2, 5, 'Hemofarm', 'E23TR', 'PILL', 'aspirin', 'nema', 'NO_PRESCRIPTION', 'analgetik', 2);
+	
+INSERT INTO public.medicine(
+	medicine_id, loyalty_points, manufacturer, code, medicine_form, name, precautions, prescription_type, type, medicine_specification_med_spec_id)
+	VALUES (3, 10, 'Galenika', 'G567H', 'PILL', 'kafetin', 'nema', 'NO_PRESCRIPTION', 'analgetik', 3);
+	
+INSERT INTO public.allergy(
+	allergy_id)
+	VALUES (1);
+	
+INSERT INTO public.allergy_medicine(
+	allergy_allergy_id, medicine_medicine_id)
+	VALUES (1, 1);
+
+INSERT INTO public."user"(
+	dtype, user_id, email, first_name, last_name, password, phone_number, username, user_type, rating, penalty, address_id, pharmacy_pharmacy_id, allergy_allergy_id)
+	VALUES ('Patient', 1, 'milica98@gmai.com', 'Milica', 'Lukic', 'm_98', '065/897-87-65', 'mimi', 'PATIENT', null, 0, 1, null, 1);
+	
+INSERT INTO public.loyalty_card(
+	card_id, loyalty_category, points, patient_user_id)
+	VALUES (1, 'GOLD', 40, 1);
+
+INSERT INTO public."user"(
+	dtype, user_id, email, first_name, last_name, password, phone_number, username, user_type, rating, penalty, address_id, pharmacy_pharmacy_id, allergy_allergy_id)
+	VALUES ('Supplier',2, 'ivanagavric@gmai.com', 'Ivana', 'Gavric', 'ivana', '0657894123', 'null', 'SUPPLIER', null, 0, 2, null, null);
+
+INSERT INTO public."user"(
+	dtype, user_id, email, first_name, last_name, password, phone_number, username, user_type, rating, penalty, address_id, pharmacy_pharmacy_id, allergy_allergy_id)
+	VALUES ('Pharmacist', 3, 'isateam39@gmail.com', 'Nikola', 'Nikolic', '123', '061/000-25-10', 'nikola123', 'PHARMACIST', 7.2, null, 3, 1, null);
