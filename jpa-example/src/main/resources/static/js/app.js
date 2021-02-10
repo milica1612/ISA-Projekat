fb79016a69a2b339745fd7236d2943e3733a0ad8
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-02-10 22:51:15 +0100

    feat: added init pharmacy profile

--- jpa-example/src/main/resources/static/js/app.js
+++ jpa-example/src/main/resources/static/js/app.js
@@ -9,6 +9,7 @@ const patient = {template: '<patient></patient>'}
 const seeOffers = {template: '<seeOffers></seeOffers>'}
 const registerPhAdmin = {template: '<registerPhAdmin></registerPhAdmin>'}
 const registerDerm = {template: '<registerDerm></registerDerm>'}
+const pharmacyProfile = {template: '<pharmacyProfile></pharmacyProfile>'}
 
 const router = new VueRouter({
 	mode: 'hash',
@@ -23,7 +24,8 @@ const router = new VueRouter({
 		{path: '/patient', component: patient},
 		{path: '/seeOffers', component: seeOffers},
 		{path: '/registerPhAdmin', component: registerPhAdmin},
-		{path: '/registerDerm', component: registerDerm}
+		{path: '/registerDerm', component: registerDerm},
+		{path: '/pharmacyProfile', component: pharmacyProfile}
 	]
 });
 
