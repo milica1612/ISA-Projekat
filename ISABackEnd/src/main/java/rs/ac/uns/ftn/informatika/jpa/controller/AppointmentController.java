7b72fb01cc565adf3c7af4af18561ae444b47019
zorana98 <lazic.ra53.2017@uns.ac.rs>
2021-05-21 14:37:06 +0200

    refactor: crossOrigin

--- ISABackEnd/src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/AppointmentController.java
+++ ISABackEnd/src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/AppointmentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,7 @@ import rs.ac.uns.ftn.informatika.jpa.model.Patient;
 import rs.ac.uns.ftn.informatika.jpa.model.User;
 import rs.ac.uns.ftn.informatika.jpa.service.AppointmentService;
 
-
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping(value = "/appointments", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AppointmentController {
