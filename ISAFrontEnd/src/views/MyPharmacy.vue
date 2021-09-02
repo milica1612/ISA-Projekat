adf4614177a8ed96621aaa1b9ce9816860f0abe1
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-09-02 23:23:56 +0200

    feat: added list of medicine items that the pharmacy has in stock
    On pharmacy page - pharmacy admin

--- ISAFrontEnd/src/views/MyPharmacy.vue
+++ ISAFrontEnd/src/views/MyPharmacy.vue
@@ -71,6 +71,26 @@
           </v-form>
         </v-card-text>
       </v-card>
+      <v-spacer></v-spacer>
+      <v-card id="medicinesInPharmacyCard">
+        <div>
+          <v-data-table
+            :headers="medicinesHeaders"
+            :items="medicinesInPharmacy"
+            sort-by="quantity"
+          >
+            <template v-slot:top>
+              <v-toolbar dense dark color="light-blue darken-2">
+                <v-spacer></v-spacer>
+                <v-toolbar-title class="text-center">
+                  All medicine items in {{name}} pharmacy
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+            </template>
+          </v-data-table>
+        </div>
+      </v-card>
     </div>
   </div>
 </template>
@@ -79,39 +99,98 @@
 export default {
   name: "MyPharmacy",
   data: () => ({
-      opacity: 0.9,
-      name: '',
-      addressStreet: '',
-			streetNumber: '',
-			city: '',
-			country: '',
-			rating: '',
-			description: '',
-			pharmacy: null,
+    opacity: 0.9,
+    name: "",
+    addressStreet: "",
+    streetNumber: "",
+    city: "",
+    country: "",
+    rating: "",
+    description: "",
+    pharmacy: null,
+    medicinesInPharmacy: [],
+    medicinesHeaders: [
+      {
+        text: "Medicine code",
+        value: "medicineCode",
+        align: "center",
+        sortable: true,
+      },
+      {
+        text: "Medicine name",
+        value: "name",
+        align: "center",
+        sortable: true,
+      },
+      {
+        text: "Medicine type",
+        value: "type",
+        align: "center",
+        sortable: true,
+      },
+      {
+        text: "Medicine form",
+        value: "medicineForm",
+        align: "center",
+        sortable: true,
+      },
+      {
+        text: "Prescription type",
+        value: "prescriptionType",
+        align: "center",
+        sortable: true,
+      },
+      {
+        text: "Quantity",
+        value: "quantity",
+        align: "center",
+        sortable: true,
+      },
+    ],
   }),
   mounted() {
-    this.axios
-      .get(
-        "http://localhost:8091/pharmacy/getPharmacyById/" +
-          localStorage.getItem("pharmacyId"),
-        {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response.data);
-        this.myPharmacy = response.data;
-				this.name = response.data.name;
-				this.addressStreet = response.data.street;
-				this.streetNumber = response.data.streetNumber;
-				this.city = response.data.city;
-				this.country = response.data.country;
-				this.rating = response.data.rating;
-				this.description = response.data.description;
-      })
-      .catch((err) => console.log(err));
+    this.initialize();
+  },
+  methods: {
+    initialize() {
+      this.axios
+        .get(
+          "http://localhost:8091/pharmacy/getPharmacyById/" +
+            localStorage.getItem("pharmacyId"),
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          this.myPharmacy = response.data;
+          this.name = response.data.name;
+          this.addressStreet = response.data.street;
+          this.streetNumber = response.data.streetNumber;
+          this.city = response.data.city;
+          this.country = response.data.country;
+          this.rating = response.data.rating;
+          this.description = response.data.description;
+        })
+        .catch((err) => console.log(err));
+
+      this.axios
+        .get(
+          "http://localhost:8091/medicineItem/findMedicineItemsByPharmacy/" +
+            localStorage.getItem("pharmacyId"),
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          this.medicinesInPharmacy = response.data;
+        });
+    },
   },
 };
 </script>
@@ -125,7 +204,14 @@ export default {
   font-weight: bold;
 }
 #pharmacyInfoCard {
-  width: 40%;
+  width: 60%;
   margin: auto;
+}
+#medicinesInPharmacyCard {
+  width: 60%;
+  text-align: center;
+  margin: auto;
+  margin-top: 5%;
+  opacity: 0.9;
 }
 </style>
